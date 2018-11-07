bc587792d324f5ac60aac90046a0c03ab3e96908
joao pedro <joao.cabral@gmail.com>
2018-11-07 14:36:20 -0200

    update url

--- restful-apis-example/core/urls.py
+++ restful-apis-example/core/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.github, name='github'),
-    path('teste/', views.api_tipos, name='api_tipos'),
+    path('status/', views.api_tipos, name='api_tipos'),
 ]
